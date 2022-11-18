b63b70d9fbce18bd004ffe52f1464c8cf79d4e0b
Simon Muller <samullers@gmail.com>
2022-11-18 23:30:31 +0200

    Move utility test functions into separate file

--- tests/test_log_rate_limit.py
+++ tests/test_log_rate_limit.py
@@ -1,22 +1,10 @@
 import time
-import inspect
 import logging
 from unittest.mock import patch
 
 from log_rate_limit import StreamRateLimitFilter, RateLimit
 
-
-def get_test_name():
-    """Return the function name of the test that called this function."""
-    # Stack[0] refers to this function, while stack[1] refers to the function (one higher in the stack) that called it.
-    return inspect.stack()[1].function
-
-
-def generate_lines(count):
-    lines = []
-    for i in range(count):
-        lines.append(f"Line {i+1}")
-    return lines
+from utils import get_test_name, generate_lines
 
 
 def test_log_limit_default_unaffected(caplog) -> None:
---
+++ tests/utils.py
@@ -0,0 +1,15 @@
+"""Utility functions used in the tests."""
+import inspect
+
+
+def get_test_name():
+    """Return the function name of the test that called this function."""
+    # Stack[0] refers to this function, while stack[1] refers to the function (one higher in the stack) that called it.
+    return inspect.stack()[1].function
+
+
+def generate_lines(count):
+    lines = []
+    for i in range(count):
+        lines.append(f"Line {i+1}")
+    return lines
